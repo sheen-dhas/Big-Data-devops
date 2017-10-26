dafddb0435160cd0ab240d94afbd3979b63615fd
sheen-dhas <31241211+sheen-dhas@users.noreply.github.com>
2017-10-26 12:27:55 +0530

    Add files via upload

---
+++ gp_connect/script_gp.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+/usr/local/greenplum-db/bin/gpstate	
+/usr/local/greenplum-db/bin/psql -c "select current_date" > /home/gpadmin/date_tst.log
+echo /home/gpadmin/date_tst.log
+exit ;		
