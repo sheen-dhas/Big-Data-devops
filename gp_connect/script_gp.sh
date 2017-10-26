818a58fd0bc6e20d764c75da1306a49b7d4df460
sheen-dhas <31241211+sheen-dhas@users.noreply.github.com>
2017-10-26 13:01:47 +0530

    Update script_gp.sh

--- gp_connect/script_gp.sh
+++ gp_connect/script_gp.sh
@@ -1,6 +1,6 @@
 
 
-/usr/local/greenplum-db/bin/gpstate	
-/usr/local/greenplum-db/bin/psql -c "select current_date" > /home/gpadmin/date_tst.log
+su -u gpadmin /usr/local/greenplum-db/bin/gpstate	
+su -u gpadmin /usr/local/greenplum-db/bin/psql -c "select current_date" > /home/gpadmin/date_tst.log
 echo /home/gpadmin/date_tst.log
 exit ;		
